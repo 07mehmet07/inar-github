73b52548ebfd619fa91d3fc590b2601ab348d3a5
07mehmet07 <wwwbjk3@gmail.com>
2023-08-08 21:40:05 +0300

    assignments01

---
+++ src/week_01/assignment/Question_01_01.java
@@ -0,0 +1,14 @@
+package week_01.assignment;
+
+public class Question_01_01 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" welcome to java ");
+        System.out.println(" welcome to computer science");
+        System.out.println(" programing is funny");
+    }
+
+
+
+}
---
+++ src/week_01/assignment/Question_01_02.java
@@ -0,0 +1,13 @@
+package week_01.assignment;
+
+public class Question_01_02 {
+
+    public static void main(String[] args) {
+
+        System.out.println("welcome to java");
+        System.out.println("welcome to java");
+        System.out.println("welcome to java");
+        System.out.println("welcome to java");
+        System.out.println("welcome to java");
+    }
+}
