fae099d3982addd01f9cdec5e2ac728b75f0e3f8
07mehmet07 <wwwbjk@gmail.com>
2023-12-11 00:27:55 +0300

    last week for java

--- src/week_14/assigment/Question_14_10.java
+++ src/week_14/assigment/Question_14_10.java
@@ -1,4 +1,45 @@
 package week_14.assigment;
 
+import java.util.Stack;
+
 public class Question_14_10 {
+    public static void main(String[] args) {
+
+        String prifix = "1 2 + 3 *";
+        String[] list = prifix.split("\\s+");
+        int result = prifixExplanation(list);
+        System.out.println("Output : " + result);
+        System.out.printf("Explanation ((%s %s %s) %s %s ) = %s", list[0], list[2], list[1], list[4], list[3],result);
+
+    }
+
+    private static int prifixExplanation(String[] list) {
+        Stack<Integer> stack = new Stack<>();
+        for (String symbol : list) {
+            if (symbol.matches("-?\\d+")) {
+                stack.push(Integer.parseInt(symbol));
+            } else {
+                int operand2 = stack.pop();
+                int operand1 = stack.pop();
+
+                switch (symbol) {
+                    case "+":
+                        stack.push(operand2 + operand1);
+                        break;
+                    case "-":
+                        stack.push(operand1 - operand2);
+                        break;
+                    case "*":
+                        stack.push(operand1 * operand2);
+                        break;
+                    case "/":
+                        stack.push(operand1 / operand2);
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Invalid Operand!");
+                }
+            }
+        }
+        return stack.pop();
+    }
 }
