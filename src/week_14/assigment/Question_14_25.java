a9d956ea52fe2bcd145a11ffcb71609ff71b1644
07mehmet07 <wwwbjk@gmail.com>
2023-12-12 12:53:53 +0300

    java

---
+++ src/week_14/assigment/Question_14_25.java
@@ -0,0 +1,44 @@
+package week_14.assigment;
+
+import java.util.*;
+
+public class Question_14_25 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 15; i++) {
+            list.add((int) (Math.random() * 100 + 1));
+        }
+        System.out.println(list);
+        int generated = (int) (Math.random() * 100 + 1);
+        System.out.println("Generated Target : " + generated);
+        ArrayList<Integer> numbers = sumOfNumbers(list, generated);
+        if (numbers.size() == 2) {
+            System.out.println("Number are : " + numbers.get(0) + " , " + numbers.get(1));
+            System.out.println("Result : True");
+        } else {
+            System.out.println("There are not these two numbers");
+            System.out.println("Result : False");
+        }
+
+
+    }
+
+    private static ArrayList<Integer> sumOfNumbers(List<Integer> list, int generated) {
+        Set<Integer> sumOfList = new HashSet<>(list);
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int nums : sumOfList) {
+            int complement = generated - nums;
+            if (sumOfList.contains(complement)) {
+                numbers.add(nums);
+                numbers.add(complement);
+                return numbers;
+            }
+
+        }
+        return numbers;
+
+    }
+
+
+}
